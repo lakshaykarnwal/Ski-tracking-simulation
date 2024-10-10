26946abaf1d99dc157d84dc01c92d626ebea8102
lakshaykarnwal <lakshaykarnwal@gmail.com>
2024-10-10 16:17:42 -0700

    updated cypress tests
    updated cypress tests

--- src/components/ArcGISMap.cy.js
+++ src/components/ArcGISMap.cy.js
@@ -5,34 +5,28 @@ describe('ArcGISMap Tests', () => {
   let recenterMapSpy;
 
   beforeEach(() => {
-    // Create a spy for the recenterMap method
     recenterMapSpy = cy.spy();
-
-    // Mount the component and replace the recenterMap method with the spy
     mount(ArcGISMap, {
       methods: {
         recenterMap: recenterMapSpy,
       },
     });
 
-    // Log the body HTML to inspect rendering
     cy.get('body').then((body) => {
-      console.log(body.html()); // Check what elements are rendered
+      console.log(body.html());
     });
   });
 
   it('should call recenterMap when the Re-center button is clicked', () => {
     console.log('Attempting to find the Re-center button.');
 
-    // Check if the "Re-center" button with the ID 'recenterBtn' is visible and click it
-    cy.get('#recenterBtn', { timeout: 10000 }) // Using ID selector
+    cy.get('#recenterBtn', { timeout: 10000 })
       .should('be.visible')
-      .and('not.be.disabled') // Ensure the button is not disabled
+      .and('not.be.disabled')
       .click()
       .then(() => {
         console.log('Re-center button clicked.');
 
-        // Ensure the recenterMap method was called
         expect(recenterMapSpy).to.have.been.called;
       });
   });
